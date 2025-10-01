99c9e77b8adafc677cef432454c95e7e74c4514c
Dilip <dilipjava60@gmail.com>
2025-10-01 15:09:54 +0530

    Add files via upload

---
+++ src/main/java/com/example/fileusers/FileUsersApplication.java
@@ -0,0 +1,11 @@
+package com.example.fileusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUsersApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FileUsersApplication.class, args);
+    }
+}
